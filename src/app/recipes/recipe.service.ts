ce6ee4b9feccc915013571f16711a5d19f9bae0c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-08-27 19:29:18 +0200

    Store Recipes and getting them with the Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Third Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
 }
