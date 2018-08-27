6ff9fbe620a8d1fcde3bdae236eb8ff3d9211b08
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-08-27 20:23:50 +0200

    Recipe has now its own ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,28 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Third Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Naleśniki',
+      'Dla dwóch osób',
+      'https://static.smaker.pl/photos/b/b/1/bb12c4f1be32ea8632028d3f402f421f_89088_59312e3b656aa_wm.jpg',
+      [
+        new Ingredient('Jajka', 2),
+        new Ingredient('Mąka', 1)
+      ]),
+    new Recipe(
+      'Jajecznica',
+      'Dla jednej osoby',
+      'https://www.zajadam.pl/wp-content/uploads/2015/11/jajecznica-3-2-891x500.jpg',
+      [
+        new Ingredient('Jajka', 4),
+        new Ingredient('Masło', 1),
+        new Ingredient('Sól', 1)
+      ])
   ];
 
   getRecipes(): Recipe[] {
