29ec7b4fded46bffb9971e280ff07f1961de5cab
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-09-23 20:48:50 +0200

    Registered FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -31,7 +32,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   ],
   imports: [
     BrowserModule,
-    AppRountingModule
+    AppRountingModule,
+    FormsModule,
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
