88dc142b6d05615c633ec61ac4075ab77ea545fc
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-08 17:01:53 +0200

    Added Ingredients entity

---
+++ ShoppingList.Entities/Ingredients.cs
@@ -0,0 +1,17 @@
+﻿namespace ShoppingList.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Ingredients : BaseEntity
+    {
+        public Ingredients()
+        {
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
