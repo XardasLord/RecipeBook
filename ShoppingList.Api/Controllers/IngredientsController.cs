89f06b32a0d391099bf2f1a640d6ada62eb226ba
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-08 17:38:29 +0200

    Created the Ingeriends Controller and endpoint to get all ingeriends.

---
+++ ShoppingList.Api/Controllers/IngredientsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingList.Business;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly IIngredientsQuery _ingredientsQuery;
+
+        public IngredientsController(IIngredientsQuery ingredientsQuery)
+        {
+            _ingredientsQuery = ingredientsQuery;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _ingredientsQuery.GetAllAsync());
+        }
+    }
+}
