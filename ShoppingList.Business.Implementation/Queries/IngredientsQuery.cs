e10116643c87a5af810262aa2b8ce2a3eac329e1
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-08 17:33:58 +0200

    Implemented a query which gets all ingeriends from DB which are not deleted. Async operation.

---
+++ ShoppingList.Business.Implementation/Queries/IngredientsQuery.cs
@@ -0,0 +1,26 @@
+﻿namespace ShoppingList.Business.Implementation.Queries
+{
+    using Microsoft.EntityFrameworkCore;
+    using ShoppingList.Database;
+    using ShoppingList.Entities;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class IngredientsQuery : IIngredientsQuery
+    {
+        private readonly ShoppingListDbContext _shoppingListDbContext;
+
+        public IngredientsQuery(ShoppingListDbContext shoppingListDbContext)
+        {
+            _shoppingListDbContext = shoppingListDbContext;
+        }
+
+        public async Task<IEnumerable<Ingredients>> GetAllAsync()
+        {
+            return await _shoppingListDbContext.Ingredients
+                .Where(x => !x.IsDeleted)
+                .ToListAsync();
+        }
+    }
+}
---
+++ ShoppingList.Business/Queries/IIngredientsQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingList.Business
+{
+    using ShoppingList.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IIngredientsQuery
+    {
+        Task<IEnumerable<Ingredients>> GetAllAsync();
+    }
+}
