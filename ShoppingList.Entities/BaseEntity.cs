30afff3193f898f211280b447b44358370d743e4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-08 17:01:39 +0200

    Added BaseEntity with the generally available columns in DB

---
+++ ShoppingList.Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingList.Entities
+{
+    using System;
+
+    public class BaseEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
