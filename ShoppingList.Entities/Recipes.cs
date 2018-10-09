20435921b3befbad43b6e3fe0edec209fbf2eaf1
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-09 16:44:37 +0200

    Added Recipes entity

---
+++ ShoppingList.Entities/Recipes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingList.Entities
+{
+    public class Recipes : BaseEntity
+    {
+        public Recipes()
+        {
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
