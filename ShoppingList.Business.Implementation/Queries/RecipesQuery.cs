bf8360257f783892dca0e9f90b8eb7a38fb02c29
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-09 16:55:57 +0200

    Added recipe query service which allows to get all recipes from db

---
+++ ShoppingList.Business.Implementation/Queries/RecipesQuery.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.Business.Queries;
+using ShoppingList.Database;
+using ShoppingList.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Business.Implementation.Queries
+{
+    public class RecipesQuery : IRecipesQuery
+    {
+        private readonly ShoppingListDbContext _shoppingListDbContext;
+
+        public RecipesQuery(ShoppingListDbContext shoppingListDbContext)
+        {
+            _shoppingListDbContext = shoppingListDbContext;
+        }
+
+        public async Task<IEnumerable<Recipes>> GetAllAsync()
+        {
+            return await _shoppingListDbContext.Recipes
+                .Where(x => !x.IsDeleted)
+                .ToListAsync();
+        }
+    }
+}
---
+++ ShoppingList.Business/Queries/IRecipesQuery.cs
@@ -0,0 +1,11 @@
+﻿using ShoppingList.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Business.Queries
+{
+    public interface IRecipesQuery
+    {
+        Task<IEnumerable<Recipes>> GetAllAsync();
+    }
+}
