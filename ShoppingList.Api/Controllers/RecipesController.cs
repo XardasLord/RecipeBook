cde3ac3bfb6e10e129842b5181076f7dfeae11f9
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-09 17:06:29 +0200

    Created the Recipes Controller and endpoint to get all recipes

---
+++ ShoppingList.Api/Controllers/RecipesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingList.Business.Queries;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipesController : ControllerBase
+    {
+        private readonly IRecipesQuery _recipesQuery;
+
+        public RecipesController(IRecipesQuery recipesQuery)
+        {
+            _recipesQuery = recipesQuery;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _recipesQuery.GetAllAsync());
+        }
+    }
+}
