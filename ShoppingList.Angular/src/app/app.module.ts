c3482dd780a13040b01b0ad30302b04f1551d2f2
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-09 16:38:09 +0200

    Added ingredients service in the frontend

--- ShoppingList.Angular/src/app/app.module.ts
+++ ShoppingList.Angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -34,7 +35,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     BrowserModule,
     AppRountingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
