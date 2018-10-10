a0a248b124629e99d7ef8f5553613ecae8499de3
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-10 21:22:46 +0200

    Added mappers

---
+++ ShoppingList.Api/Mappings/DomainProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ShoppingList.Business.Models;
+using ShoppingList.Entities;
+
+namespace ShoppingList.Api.Mappings
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile()
+        {
+            CreateMap<Ingredients, IngredientModel>().ReverseMap();
+            CreateMap<Recipes, RecipeModel>().ReverseMap();
+        }
+    }
+}
