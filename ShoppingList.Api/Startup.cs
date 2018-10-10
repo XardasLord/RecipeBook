21db37d2f82377312c1957cb2bf4158ea69b8664
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-10 21:21:02 +0200

    Registered AutoMapper in the container

--- ShoppingList.Api/Startup.cs
+++ ShoppingList.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -38,6 +39,7 @@ namespace ShoppingList.Api
             services.AddTransient<IIngredientsQuery, IngredientsQuery>();
             services.AddTransient<IRecipesQuery, RecipesQuery>();
 
+            services.AddAutoMapper();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
