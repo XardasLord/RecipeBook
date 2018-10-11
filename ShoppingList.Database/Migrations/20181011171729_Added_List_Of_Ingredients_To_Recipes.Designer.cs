7bd3bae52756a04c541a8101a879a7e35f48e692
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-11 19:26:13 +0200

    Created migration -> "Added_List_Of_Ingredients_To_Recipes" (Lazy Loading using virtual ICollection<T>)

--- ShoppingList.Database/Migrations/20181011171729_Added_List_Of_Ingredients_To_Recipes.Designer.cs
+++ ShoppingList.Database/Migrations/20181011171729_Added_List_Of_Ingredients_To_Recipes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingList.Database;
 
 namespace ShoppingList.Database.Migrations
 {
     [DbContext(typeof(ShoppingListDbContext))]
-    partial class ShoppingListDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181011171729_Added_List_Of_Ingredients_To_Recipes")]
+    partial class Added_List_Of_Ingredients_To_Recipes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
