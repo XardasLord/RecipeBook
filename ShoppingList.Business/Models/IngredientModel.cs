b2925aae1972055e0d04861d5d1473947b507116
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-14 10:47:16 +0200

    Added Ids to the model which is returning to the client

--- ShoppingList.Business/Models/IngredientModel.cs
+++ ShoppingList.Business/Models/IngredientModel.cs
@@ -1,7 +1,10 @@
-﻿namespace ShoppingList.Business.Models
+﻿using System;
+
+namespace ShoppingList.Business.Models
 {
     public class IngredientModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public int Amount { get; set; }
     }
--- ShoppingList.Business/Models/RecipeModel.cs
+++ ShoppingList.Business/Models/RecipeModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ShoppingList.Business.Models
 {
     public class RecipeModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
