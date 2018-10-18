2935b311c872cff83cfac836649099919260a5df
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-18 18:46:19 +0200

    Created RecipePart entity which will handle the ingredients for the recipes in DB

---
+++ ShoppingList.Entities/RecipePart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingList.Entities
+{
+    public class RecipePart : BaseEntity
+    {
+        public RecipePart()
+        {
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public Recipe Recipe { get; set; }
+        // Probably some unit's dictionary later?
+        public string Unit { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
