024d82e3336addb31e4cc316bc27826b50529548
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-10-29 18:44:51 +0100

    Added empty constructor to the Recipe model

--- ShoppingList.Angular/src/app/recipes/recipe.model.ts
+++ ShoppingList.Angular/src/app/recipes/recipe.model.ts
@@ -7,7 +7,10 @@ export class Recipe {
     imageUrl: string;
     recipeParts: RecipePart[];
 
-    constructor(name: string, desc: string, imageUrl: string, recipeParts: RecipePart[]) {
+    constructor();
+    // tslint:disable-next-line:unified-signatures
+    constructor(name?: string, desc?: string, imageUrl?: string, recipeParts?: RecipePart[]);
+    constructor(name?: string, desc?: string, imageUrl?: string, recipeParts?: RecipePart[]) {
         this.name = name;
         this.description = desc;
         this.imageUrl = imageUrl;
