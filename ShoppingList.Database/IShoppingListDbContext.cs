ae559f1f83bc374e34aa3b64ef5984b134215a8a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-11-16 17:00:23 +0100

    Created an extension method for the IApplicationBuilder which updates the database when the new migration is available. When database is not created it creates it also.

--- ShoppingList.Database/IShoppingListDbContext.cs
+++ ShoppingList.Database/IShoppingListDbContext.cs
@@ -1,14 +1,17 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using ShoppingList.Entities;
 
 namespace ShoppingList.Database
 {
-    public interface IShoppingListDbContext
+    public interface IShoppingListDbContext : IDisposable
     {
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        DatabaseFacade Database { get; }
 
         DbSet<Recipe> Recipes { get; set; }
         DbSet<RecipePart> RecipeParts { get; set; }
