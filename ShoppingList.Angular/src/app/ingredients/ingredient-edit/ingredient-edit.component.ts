014f836e29bddc8a505e651a65db47fcc02a5cf2
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-12-06 21:31:29 +0100

    Handled arror if ingredient adding fails

--- ShoppingList.Angular/src/app/ingredients/ingredient-edit/ingredient-edit.component.ts
+++ ShoppingList.Angular/src/app/ingredients/ingredient-edit/ingredient-edit.component.ts
@@ -31,12 +31,12 @@ export class IngredientEditComponent implements OnInit {
     const ingredientToAdd = new Ingredient(this.ingredientForm.value['name']);
 
     this.ingredientService.add(ingredientToAdd).subscribe(newIngredient => {
-      // TODO: Information about successful ingredient add
-      // ...
       this.ingredientService.ingredientsChanged.next(newIngredient);
       this.ingredientForm.reset();
 
       this.toastrService.success(`${newIngredient.name} ingredient has been added!`, `New ingredient`);
-    });
+    },
+      error => alert(`An error has occured while adding the ingredient: ${error.message}`)
+    );
   }
 }
