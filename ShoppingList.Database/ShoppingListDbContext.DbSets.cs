84c6975329798372279fce1e5ebc0e145c449df0
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-01-04 18:09:02 +0100

    Created migration - Added_Users_Table

--- ShoppingList.Database/ShoppingListDbContext.DbSets.cs
+++ ShoppingList.Database/ShoppingListDbContext.DbSets.cs
@@ -8,5 +8,6 @@ namespace ShoppingList.Database
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<RecipePart> RecipeParts { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
