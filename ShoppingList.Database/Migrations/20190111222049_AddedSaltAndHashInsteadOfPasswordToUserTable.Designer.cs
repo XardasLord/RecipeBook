2921eb6025125fd65908d8f69d462ece73f0739c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-01-11 23:38:36 +0100

    Created migration `AddedSaltAndHashInsteadOfPasswordToUserTable`

--- ShoppingList.Database/Migrations/20190111222049_AddedSaltAndHashInsteadOfPasswordToUserTable.Designer.cs
+++ ShoppingList.Database/Migrations/20190111222049_AddedSaltAndHashInsteadOfPasswordToUserTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingList.Database;
 
 namespace ShoppingList.Database.Migrations
 {
     [DbContext(typeof(ShoppingListDbContext))]
-    partial class ShoppingListDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190111222049_AddedSaltAndHashInsteadOfPasswordToUserTable")]
+    partial class AddedSaltAndHashInsteadOfPasswordToUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
