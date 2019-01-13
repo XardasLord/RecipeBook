cf54be21482a13054bca539ad58bb1949c09f266
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-01-13 13:38:18 +0100

    Added alert message only if Unauthorizate

--- RecipeBook.Angular/src/app/http-error.interceptor.ts
+++ RecipeBook.Angular/src/app/http-error.interceptor.ts
@@ -17,11 +17,12 @@ export class HttpErrorInterceptor implements HttpInterceptor {
             // server-side error
             if (error.status === 401) {
               errorMessage = `You have to log in before do this action`;
+              window.alert(errorMessage);
             } else {
               errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
             }
           }
-          window.alert(errorMessage);
+          // window.alert(errorMessage);
           return throwError(errorMessage);
         })
       );
