13dabc332c32e6f121876595ef07dd16b6d1772a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-01-13 15:12:30 +0100

    Created the very based CoreModule and registered it in AppModule

--- RecipeBook.Angular/src/app/app-routing.module.ts
+++ RecipeBook.Angular/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
--- RecipeBook.Angular/src/app/app.module.ts
+++ RecipeBook.Angular/src/app/app.module.ts
@@ -5,17 +5,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtModule } from '@auth0/angular-jwt';
 import { ToastrModule } from 'ngx-toastr';
 
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthGuard } from './auth/guards/auth-guard.service';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRountingModule } from './app-routing.module';
 import { HttpErrorInterceptor } from './http-error.interceptor';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { HomeComponent } from './home/home.component';
 
 export function tokenGetter() {
   return localStorage.getItem('jwt');
@@ -23,9 +22,7 @@ export function tokenGetter() {
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +33,7 @@ export function tokenGetter() {
     ShoppingListModule,
     AuthModule,
     SharedModule,
+    CoreModule,
     AppRountingModule,
     ToastrModule.forRoot(),
     JwtModule.forRoot({
