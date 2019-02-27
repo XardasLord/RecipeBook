a26987672360cdbee7cd84b05b0e273583c19a15
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-02-27 16:14:20 +0100

    Created unit test for GetAllRecipesQueryHandler to check if it returns correct type when query is valid

--- RecipeBook.Business.Tests.Unit/Recipes/GetAllRecipesQueryHandlerTests.cs
+++ RecipeBook.Business.Tests.Unit/Recipes/GetAllRecipesQueryHandlerTests.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using FluentAssertions;
+using MockQueryable.Moq;
 using Moq;
 using NUnit.Framework;
 using RecipeBook.Business.Implementation.Recipes.Queries.GetAllRecipes;
 using RecipeBook.Business.Models;
 using RecipeBook.Database;
+using RecipeBook.Entities;
 
 namespace RecipeBook.Business.Tests.Unit.Recipes
 {
@@ -38,6 +41,22 @@ namespace RecipeBook.Business.Tests.Unit.Recipes
             Func<Task> result = async () => await Act(query);
 
             result.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public async Task GetAllRecipesQueryHandler_WithValidQuery_ReturnListOfRecipesTypeAsync()
+        {
+            var query = new GetAllRecipesQuery();
+            var recipes = new List<Recipe>();
+            var recipesMock = recipes.AsQueryable().BuildMockDbSet();
+
+            _context.Setup(x => x.Recipes).Returns(recipesMock.Object);
+            _mapper.Setup(x => x.Map<IEnumerable<RecipeModel>>(It.IsAny<IEnumerable<Recipe>>()))
+                .Returns(new List<RecipeModel>());
+
+            var result = await Act(query);
+
+            result.Should().BeOfType<List<RecipeModel>>();
         }
     }
 }
