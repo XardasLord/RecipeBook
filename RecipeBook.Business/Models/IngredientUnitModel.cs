e9a37d825fa1fecb605b41f8b1e7a2fad38e4575
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-02-28 09:22:14 +0100

    Created an IngredientUnitModel which is the ViewModel

---
+++ RecipeBook.Business/Models/IngredientUnitModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RecipeBook.Business.Models
+{
+    public class IngredientUnitModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
